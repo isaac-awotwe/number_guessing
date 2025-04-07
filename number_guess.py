876d5b3ca6e37fa3eedf9c60eb56aed8f1cac721
Isaac Awotwe <isaacawotwe@gmail.com>
2025-04-06 23:12:36 -0600

    Update game script

---
+++ number_guess.py
@@ -0,0 +1,45 @@
+# import packages
+import random
+from art import logo
+
+
+print(logo)
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100.")
+number = random.randint(1, 100)
+print(number)
+
+difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+
+if difficulty == 'easy':
+    attempts = 10
+elif difficulty == 'hard':
+    attempts = 5
+
+# print(attempts)
+
+# def wrong_guess(attempts_left, num_guesses):
+
+print(f"You have {attempts} attempts remaining to guess the number.")
+game_on = True
+
+while game_on:
+    guess = int(input("Make a guess: "))
+    if guess != number:
+        attempts-=1
+        if attempts == 0:
+            game_on = False
+            print("You've run out of guesses. Refresh the page to run again.")
+        else:
+            if guess > number:
+                print("Too high.")
+                print("Guess again.")
+            elif guess < number:
+                print("Too low.")
+                print("Guess again")
+            print(f"You have {attempts} attempts remaining.")
+    else:
+        game_on = False
+        print(f"You got it! The answer was {number}.")
+
+
