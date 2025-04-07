d87b47cd72638241566a1b38cc12c14e5a77eb89
Isaac Awotwe <isaacawotwe@gmail.com>
2025-04-07 15:28:36 -0600

    Update game script

--- number_guess.py
+++ number_guess.py
@@ -8,7 +8,7 @@ print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
 number = random.randint(1, 100)
 
-difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
 
 if difficulty == 'easy':
     attempts = 10
